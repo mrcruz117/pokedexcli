e10f0122dd9e0aa6ba84e9469eb35ddaa9968fef
mrcruz117 <mrcruz1989@gmail.com>
2024-06-15 18:24:27 -0400

    first logs

--- main.go
+++ main.go
@@ -1,9 +1,68 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func commandHelp() error {
+	fmt.Println("Available commands:")
+	fmt.Println("help - Displays a help message")
+	fmt.Println("exit - Exit the Pokedex")
+	return nil
+}
+
+func commandExit() error {
+	fmt.Println("Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func startPokedex() {
+	commands := map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Type 'help' to see a list of commands")
+
+	for {
+		fmt.Print("> ")
+		reader := bufio.NewReader(os.Stdin)
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+		command, ok := commands[input]
+		if !ok {
+			fmt.Println("Unknown command")
+			continue
+		}
+		err := command.callback()
+		if err != nil {
+			fmt.Println("Error:", err)
+		}
+
+	}
+
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	startPokedex()
+
 }

d87db846797f9be9dd93fe7329a4a1b680fe1c8e
mrcruz117 <mrcruz1989@gmail.com>
2024-06-22 14:29:12 -0400

    getting api info

--- main.go
+++ main.go
@@ -1,85 +1,16 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
+	"time"
+
+	pokeapi "github.com/mrcruz117/pokedexcli/internal/pokeapi"
 )
 
-func cleanInput(text string) []string {
-	output := strings.ToLower(text)
-	words := strings.Fields(output)
-	return words
-}
-
-type cliCommand struct {
-	name        string
-	description string
-	callback    func() error
-}
-
-func commandHelp() error {
-	fmt.Println()
-	fmt.Println("Welcome to the Pokedex!")
-	fmt.Println("Usage:")
-	fmt.Println()
-	for _, cmd := range getCommands() {
-		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
-	}
-	fmt.Println()
-	return nil
-}
-
-func commandExit() error {
-	fmt.Println("Goodbye!")
-	os.Exit(0)
-	return nil
-}
-
-func getCommands() map[string]cliCommand {
-	return map[string]cliCommand{
-		"help": {
-			name:        "help",
-			description: "Displays a help message",
-			callback:    commandHelp,
-		},
-		"exit": {
-			name:        "exit",
-			description: "Exit the Pokedex",
-			callback:    commandExit,
-		},
-	}
-}
-
-func startRepl() {
-	reader := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex > ")
-		reader.Scan()
-
-		words := cleanInput(reader.Text())
-		if len(words) == 0 {
-			continue
-		}
-
-		commandName := words[0]
-
-		command, exists := getCommands()[commandName]
-		if exists {
-			err := command.callback()
-			if err != nil {
-				fmt.Println(err)
-			}
-			continue
-		} else {
-			fmt.Println("Unknown command")
-			continue
-		}
-	}
-}
-
 func main() {
-	startRepl()
+	pokeClient := pokeapi.NewClient(5 * time.Second)
+	cfg := &config{
+		pokeapiClient: pokeClient,
+	}
 
+	startRepl(cfg)
 }
